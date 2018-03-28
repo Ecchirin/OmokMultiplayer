6f065ffa69b24455c6577e2214d15873cc8bf31a
YUU\Alisa <limz992@gmail.com>
2018-03-28 16:18:27 +0800

    The creation of the basic framework of the multiplayer server with a simple connection test script for the client to connect.

---
+++ Server/OmokServer/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace OmokServer
+{
+    /// <summary>
+    /// The class that manage the connection within each thread
+    /// 
+    /// </summary>
+    class ConnectionThread
+    {
+        public TcpListener threadListener;
+        private static int connections = 0;
+
+        public void HandleConnection()
+        {
+            int recv;
+            byte[] data = new byte[1024];
+
+            TcpClient client = threadListener.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            connections++;
+            Console.WriteLine("New client accepted: {0} active connections",
+                              connections);
+
+            string welcome = "Welcome to my test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            ns.Write(data, 0, data.Length);
+
+            while (true)
+            {
+                data = new byte[1024];
+                recv = ns.Read(data, 0, data.Length);
+                if (recv == 0)
+                    break;
+
+                Console.WriteLine("Message received from {0}:", ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+                ns.Write(data, 0, recv);
+            }
+            ns.Close();
+            client.Close();
+            connections--;
+            Console.WriteLine("Client disconnected: {0} active connections",
+                               connections);
+        }
+    }
+
+    /// <summary>
+    /// The class will assign new connections to new threads
+    /// 
+    /// Try to use non-obsolete codes
+    /// </summary>
+    public class ThreadedTCPServer
+    {
+        private TcpListener client;
+
+        public ThreadedTCPServer()
+        {
+            //IPAddress ipAddress = Dns.Resolve("localhost").AddressList[0];
+            ////IPAddress ipAddress = Dns.GetHostAddresses(Dns.GetHostName())[0];
+            //Console.WriteLine(ipAddress.ToString());
+            //try
+            //{
+            //    client = new TcpListener(ipAddress, 13);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            client = new TcpListener(9050);
+            client.Start();
+
+            Console.WriteLine("Waiting for clients...");
+            while (true)
+            {
+                while (!client.Pending())
+                {
+                    Thread.Sleep(1000);
+                }
+
+                ConnectionThread newconnection = new ConnectionThread();
+                newconnection.threadListener = this.client;
+                Thread newthread = new Thread(new
+                          ThreadStart(newconnection.HandleConnection));
+                newthread.Start();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            ThreadedTCPServer server = new ThreadedTCPServer();
+        }
+    }
+}
---
+++ Server/OmokServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OmokServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OmokServer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b7edc11-4702-44a9-909e-3f02f2c56d0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
