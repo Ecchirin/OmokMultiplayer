97cda6323738ec90e7fe9daae68653fdf56a8263
Calvert Sim Wei Xiang <cal_9966@hotmail.com>
2018-04-05 19:34:57 +0800

    Created Lobby room selection

---
+++ Client/Omoke/Assets/Script/Room Menu/JoinRoomButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinRoomButton : MonoBehaviour {
+
+    [SerializeField]
+    string serverServiceTagName = "ServerService";
+    private ServerConnection serverService = null;
+
+    // Use this for initialization
+    void Start () {
+        serverService = GameObject.FindGameObjectWithTag(serverServiceTagName).GetComponent<ServerConnection>();
+        if (serverService == null)
+            Debug.Log("Error: Server Service cannot be found");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void JoinRoom()
+    {
+        if (serverService == null)
+            return;
+        serverService.JoinRoom(this.GetComponentInChildren<Text>().text);
+    }
+}
