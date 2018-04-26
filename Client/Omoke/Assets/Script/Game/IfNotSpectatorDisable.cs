3bca7a607e66628053a393a4052e960e9f5adc05
Calvert Sim Wei Xiang <cal_9966@hotmail.com>
2018-04-26 15:34:06 +0800

    Update build with spectator leave button when watching a match.

---
+++ Client/Omoke/Assets/Script/Game/IfNotSpectatorDisable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfNotSpectatorDisable : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] thingsToDisable = null;
+    [SerializeField]
+    string serverServiceTagName = "ServerService";
+    // Use this for initialization
+    void Start () {
+		if(!GameObject.FindGameObjectWithTag(serverServiceTagName).GetComponent<ServerConnection>().isSpectator)
+        {
+            foreach(GameObject go in thingsToDisable)
+            {
+                go.SetActive(false);
+            }
+        }
+        this.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
