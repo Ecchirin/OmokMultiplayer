05aa5044c96e78b0653fe43e65a4ed876940aba3
Calvert Sim Wei Xiang <cal_9966@hotmail.com>
2018-05-02 14:00:59 +0800

    Changes to interface class to support disconnect and get2dmapdata

---
+++ Client/For other student to connect to server/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script is used for changing scene
+/// Nothing else should be here
+/// </summary>
+
+public class SceneChange : MonoBehaviour {
+
+    //Change scene using name
+    public void ChangeScene(string levelName)
+    {
+        Debug.Log("Scene change to: " + levelName);
+        SceneManager.LoadScene(levelName);
+    }
+
+    //Application exit button
+    public void QuitApplication()
+    {
+        Debug.Log("Quitting Application...");
+        Application.Quit();
+    }
+}
