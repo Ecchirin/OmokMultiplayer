55169bb4fada67d9a4654bc9f547adc0bae4496a
Calvert Sim Wei Xiang <cal_9966@hotmail.com>
2018-05-08 12:51:36 +0800

    update to gameplay interface to match with the group idea

---
+++ Client/Omoke/Assets/Script/Game/GetPlayerNames.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetPlayerNames : MonoBehaviour {
+    [SerializeField]
+    string serverServiceTagName = "ServerService";
+    [SerializeField]
+    bool playerOne = false;
+    [SerializeField]
+    bool playerTwo = false;
+    // Use this for initialization
+    void Start () {
+        if(playerOne)
+            this.GetComponent<TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag(serverServiceTagName).GetComponent<ServerConnection>().GetPlayerOne();
+        if (playerTwo)
+            this.GetComponent<TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag(serverServiceTagName).GetComponent<ServerConnection>().GetPlayerTwo();
+        this.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
